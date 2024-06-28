25884d059fb194010fc2edec13750359954ecb95
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-06-28 12:01:39 +0200

    commit

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
+function Form({
+  members,
+  setMembers
+}) {
+  return (
+    <div className="grid gap-2">
+      {members.map((member) => <div className="flex gap-2" key={member}>
+        <span>{member}</span>
+        <button className="px-1 text-white bg-red-500 rounded" onClick={() =>
+          setMembers(() => members.filter((x) => x !== member))
+        }>Remove</button>
+      </div>)}
+    </div>
+  );
+}
+
 export default function Home() {
-  return <main>
-    <div>This is my main page</div>
-  </main>;
+  const [members, setMembers] = useState([
+    "Guillaume", "John", "Mary"
+  ]);
+
+  return <div>
+    <Form {...{
+      members,
+      setMembers
+    }} />
+  </div>;
 }
